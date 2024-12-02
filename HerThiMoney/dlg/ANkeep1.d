6779e29c08cc27d5234c0be7f9b9115a54999ac3
Austin-BG <79753790+Austin-BG@users.noreply.github.com>
2024-12-02 21:06:58 +0300

    Fixed bug

--- HerThiMoney/dlg/ANkeep1.d
+++ HerThiMoney/dlg/ANkeep1.d
@@ -4,15 +4,15 @@ INTERJECT_COPY_TRANS3 TORGAL %state% ANtorgALL_%state%
 == KELDORJ IF ~Global("ANtorgalAll","GLOBAL",0) InParty("Keldorn") InMyArea("Keldorn") !StateCheck("Keldorn",CD_STATE_NOTVALID)~ THEN @9
 == MINSCJ IF ~Global("ANtorgalAll","GLOBAL",0) InParty("Minsc") InMyArea("Minsc") !StateCheck("Minsc",CD_STATE_NOTVALID)~ THEN @0
 == YOSHJ IF ~Global("ANtorgalAll","GLOBAL",0) InParty("Yoshimo") InMyArea("Yoshimo") !StateCheck("Yoshimo",CD_STATE_NOTVALID) InParty("Minsc") InMyArea("Minsc") !StateCheck("Minsc",CD_STATE_NOTVALID)~ THEN @1
-== KORGANJ IF ~Global("ANtorgalAll","GLOBAL",0) InParty("Korgan") InMyArea("Korgan") !Dead("Korgan") !StateCheck("Korgan",CD_STATE_NOTVALID)~ THEN @2
+== KORGANJ IF ~Global("ANtorgalAll","GLOBAL",0) InParty("Korgan") InMyArea("Korgan") !StateCheck("Korgan",CD_STATE_NOTVALID)~ THEN @2
 == IMOEN2J IF ~Global("ANtorgalAll","GLOBAL",0) InParty("Imoen2") InMyArea("Imoen2") !StateCheck("Imoen2",CD_STATE_NOTVALID) InParty("Minsc") InMyArea("Minsc") !StateCheck("Minsc",CD_STATE_NOTVALID) InParty("Korgan") InMyArea("Korgan") !Dead("Korgan") !StateCheck("Korgan",CD_STATE_NOTVALID)~ THEN @3
-== MINSCJ IF ~Global("ANtorgalAll","GLOBAL",0) InParty("Minsc") InMyArea("Minsc") !StateCheck("Minsc",CD_STATE_NOTVALID) InParty("Korgan") InMyArea("Korgan") !Dead("Korgan") !StateCheck("Korgan",CD_STATE_NOTVALID) OR(3) !InParty("Imoen2") !InMyArea("Imoen2") StateCheck("Imoen2",CD_STATE_NOTVALID)~ THEN @3
-== KORGANJ IF ~Global("ANtorgalAll","GLOBAL",0) InParty("Korgan") InMyArea("Korgan") !Dead("Korgan") !StateCheck("Korgan",CD_STATE_NOTVALID) InParty("Minsc") InMyArea("Minsc") !StateCheck("Minsc",CD_STATE_NOTVALID)~ THEN @4
-== MINSCJ IF ~Global("ANtorgalAll","GLOBAL",0) InParty("Minsc") InMyArea("Minsc") !StateCheck("Minsc",CD_STATE_NOTVALID) InParty("Korgan") InMyArea("Korgan") !Dead("Korgan") !StateCheck("Korgan",CD_STATE_NOTVALID)~ THEN @5
+== MINSCJ IF ~Global("ANtorgalAll","GLOBAL",0) InParty("Minsc") InMyArea("Minsc") !StateCheck("Minsc",CD_STATE_NOTVALID) InParty("Korgan") InMyArea("Korgan") !StateCheck("Korgan",CD_STATE_NOTVALID) OR(3) !InParty("Imoen2") !InMyArea("Imoen2") StateCheck("Imoen2",CD_STATE_NOTVALID)~ THEN @3
+== KORGANJ IF ~Global("ANtorgalAll","GLOBAL",0) InParty("Korgan") InMyArea("Korgan") !StateCheck("Korgan",CD_STATE_NOTVALID) InParty("Minsc") InMyArea("Minsc") !StateCheck("Minsc",CD_STATE_NOTVALID)~ THEN @4
+== MINSCJ IF ~Global("ANtorgalAll","GLOBAL",0) InParty("Minsc") InMyArea("Minsc") !StateCheck("Minsc",CD_STATE_NOTVALID) InParty("Korgan") InMyArea("Korgan") !StateCheck("Korgan",CD_STATE_NOTVALID)~ THEN @5
 == JAHEIRAJ IF ~Global("ANtorgalAll","GLOBAL",0) InParty("Jaheira") InMyArea("Jaheira") !StateCheck("Jaheira",CD_STATE_NOTVALID) InParty("Minsc") InMyArea("Minsc") !StateCheck("Minsc",CD_STATE_NOTVALID)~ THEN @6
 == EDWINJ IF ~Global("ANtorgalAll","GLOBAL",0) InParty("Edwin") InMyArea("Edwin") !StateCheck("Edwin",CD_STATE_NOTVALID) OR(3) InParty("Mazzy") InParty("Minsc") InParty("Keldorn")~ THEN @7
 == AERIEJ IF ~Global("ANtorgalAll","GLOBAL",0) InParty("Aerie") InMyArea("Aerie") !StateCheck("Aerie",CD_STATE_NOTVALID) InParty("Minsc") InMyArea("Minsc") !StateCheck("Minsc",CD_STATE_NOTVALID)~ THEN @8
 == JAHEIRAJ IF ~Global("ANtorgalAll","GLOBAL",0) InParty("Jaheira") InMyArea("Jaheira") !StateCheck("Jaheira",CD_STATE_NOTVALID) InParty("Minsc") InMyArea("Minsc") !StateCheck("Minsc",CD_STATE_NOTVALID) OR(3) !InParty("Aerie") !InMyArea("Aerie") StateCheck("Aerie",CD_STATE_NOTVALID)~ THEN @8
-== TORGAL IF ~Global("ANtorgalAll","GLOBAL",0) InParty("Minsc") InMyArea("Minsc") !StateCheck("Minsc",CD_STATE_NOTVALID)~ THEN @10 DO ~SetGlobal("ANtorgalAll","GLOBAL",1)~
+== TORGAL @10 DO ~SetGlobal("ANtorgalAll","GLOBAL",1)~
 END
 
